81a9e8d9d089563a33ea6096b0360cffe43dbdc4
Paul Wintz <19618780+pwintz@users.noreply.github.com>
2025-09-26 18:29:54 -0700

    Update plotCircle

--- +plots/plotCircle.m
+++ +plots/plotCircle.m
@@ -1,8 +1,11 @@
-function plotCircle(center, radius, varargin)
-  theta = linspace(0, 2*pi);
-  plot(...
-    center(1) + radius*cos(theta), ...
-    center(2) + radius*sin(theta), ...
-    '-k', varargin{:}...
-  )
+function plt = plotCircle(center, radius, varargin)
+    theta = linspace(0, 2*pi);
+    x = center(1) + radius*cos(theta);
+    y = center(2) + radius*sin(theta);
+    plt = plot(x, y, '-k', varargin{:});
+
+    if nargout == 0
+        % Prevent output if function is not terminated with a semicolon.
+        clearvars this
+    end
 end
